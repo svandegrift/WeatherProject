56d560f139d044d745bc5c64c0cf8de9a51d5ff8
Steven Vandegrift <svandegrift@icloud.com>
2021-02-22 19:46:09 -0600

    Add Weather API

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const https = require('https');
 const app = express();
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Kansas City, USA &appid=31418ac2a27d1015ea90a9875d08e464&units=imperial';
 
+app.get('/', function (req, res){
+    https.get(url, function(responce){
+        console.log(responce.statusCode);
+        responce.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+            var temp = weatherData.main.temp;
+            var status = weatherData.weather[0].main;
+            console.log(`The temperature is currently ${temp} and it is ${status}`);
+        })
+    })
+    res.send("Server is up and running");
+});
 
 
 
